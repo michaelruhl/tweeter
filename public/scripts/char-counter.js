7321c78c3a926a4a17550bb2d9de438b03cc3bbe
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-20 15:29:08 +0000

    add basic tweeter design to page. Configure character counter. Add tweets.

---
+++ public/scripts/char-counter.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  console.log("ITS WORKINGGGG ITS WORKINGGG");
+
+  const tweetarea = $("#tweet-text");
+  let counter = $('#counter');
+  
+  tweetarea.on('input', () => {
+    console.log('hello')
+    const numberCount = tweetarea.val().length
+    counterCount = 140 - numberCount;
+
+      if ( counterCount < 0 ) {
+        counter.addClass('red');
+      }
+
+      if ( counterCount >= 0)  {
+        counter.removeClass('red');
+      }
+    counter.val(counterCount);
+  })
+
+
+
+});
+
+
+
