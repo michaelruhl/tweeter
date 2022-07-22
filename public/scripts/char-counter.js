89e49ca43f2adcae98be002fea3b92a9d35614fb
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-22 03:10:16 +0000

    implement dynamic char counter

--- public/scripts/char-counter.js
+++ public/scripts/char-counter.js
@@ -2,26 +2,19 @@ $(document).ready(function () {
   console.log("ITS WORKINGGGG ITS WORKINGGG");
 
   const tweetarea = $("#tweet-text");
-  let counter = $('#counter');
-  
-  tweetarea.on('input', () => {
-    console.log('hello')
-    const numberCount = tweetarea.val().length
+  let counter = $("#counter");
+
+  tweetarea.on("input", () => {
+    const numberCount = tweetarea.val().length;
     counterCount = 140 - numberCount;
 
-      if ( counterCount < 0 ) {
-        counter.addClass('red');
-      }
+    if (counterCount < 0) {
+      counter.addClass("red");
+    }
 
-      if ( counterCount >= 0)  {
-        counter.removeClass('red');
-      }
+    if (counterCount >= 0) {
+      counter.removeClass("red");
+    }
     counter.val(counterCount);
-  })
-
-
-
+  });
 });
-
-
-
